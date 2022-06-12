ecbb89ff760f58e08f4e0485e3f2ce875f4f7dab
verysetiabudi <verysetiabudi37@gmail.com>
2022-06-12 22:40:31 +0700

    fx : challenge

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,37 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallBack) => {
+  let resultArr = [];
+
+  fs.readFile(file1, { encoding: "utf8" }, (err, data) => {
+    if (err) {
+      return fnCallBack(err, null);
+    }
+
+    const dataResult = JSON.parse(data);
+    resultArr.push(dataResult.message.split(" ")[1]);
+  });
+
+  fs.readFile(file2, { encoding: "utf8" }, (err, data) => {
+    if (err) {
+      return fnCallBack(err, null);
+    }
+
+    const dataResult = JSON.parse(data);
+    resultArr.push(dataResult[0].message.split(" ")[1]);
+  });
+
+  fs.readFile(file3, { encoding: "utf8" }, (err, data) => {
+    if (err) {
+      return fnCallBack(err, null);
+    }
+
+    const dataResult = JSON.parse(data);
+    resultArr.push(dataResult[0].data.message.split(" ")[1]);
+    fnCallBack(null, resultArr);
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
